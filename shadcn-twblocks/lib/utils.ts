07ee6c00e61862e3153124b852b5762df25939e9
Federico Jorge Bonel Tozzi <federicojorgeboneltozzi@Federicos-MacBook-Pro.local>
2025-11-27 19:15:57 +0900

    chore: add utilities

--- shadcn-twblocks/lib/utils.ts
+++ shadcn-twblocks/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const adjectives = ["amazing", "new", "wonderful", "beautiful", "smart"];
+
+export function getRandomAdjective(arr = adjectives): string {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }

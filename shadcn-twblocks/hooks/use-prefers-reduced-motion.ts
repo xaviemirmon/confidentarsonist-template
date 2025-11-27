b494e278b35b54e71f09d90bf152dd5c0c90ede3
Federico Jorge Bonel Tozzi <federicojorgeboneltozzi@Federicos-MacBook-Pro.local>
2025-11-27 19:18:51 +0900

    chore: add hooks

---
+++ shadcn-twblocks/hooks/use-prefers-reduced-motion.ts
@@ -0,0 +1,35 @@
+// Adapted from https://www.joshwcomeau.com/snippets/react-hooks/use-prefers-reduced-motion/
+import * as React from "react";
+
+const QUERY = "(prefers-reduced-motion: no-preference)";
+
+const isRenderingOnServer = typeof window === "undefined";
+
+const getInitialState = () => {
+  // For our initial server render, we won't know if the user
+  // prefers reduced motion, but it doesn't matter. This value
+  // will be overwritten on the client, before any animations
+  // occur.
+  return isRenderingOnServer ? true : !window.matchMedia(QUERY).matches;
+};
+
+export function usePrefersReducedMotion() {
+  const [prefersReducedMotion, setPrefersReducedMotion] =
+    React.useState(getInitialState);
+
+  React.useEffect(() => {
+    const mediaQueryList = window.matchMedia(QUERY);
+
+    const listener = (event: MediaQueryListEvent) => {
+      setPrefersReducedMotion(!event.matches);
+    };
+
+    mediaQueryList.addEventListener("change", listener);
+
+    return () => {
+      mediaQueryList.removeEventListener("change", listener);
+    };
+  }, []);
+
+  return prefersReducedMotion;
+}
